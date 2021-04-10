3d832450c12c6c9992cad4352e95b6a92dd5c8b7
addisonhoward1 <addison.howard@hotmail.com>
2021-04-10 17:50:32 -0600

    Addison's Commit

---
+++ Models/ViewBurialsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intex2021FagElGamous.Models
+{
+
+    public class ViewBurialsViewModel
+    {
+        public List<ViewBurialsBurialModel> Burials { get; set; }
+        // Insert pagination model thing here
+        // Insert Filter model here too
+
+    }
+
+    public class ViewBurialsBurialModel
+    {
+        public string NS { get; set; }
+        public int NSTop { get; set; }
+        public int NSBottom { get; set; }
+        public string EW { get; set; }
+        public int EWTop { get; set; }
+        public int EWBottom { get; set; }
+        public string Quadrant { get; set; }
+
+        public int BurialNumber { get; set; }
+        public string OsteologyNotes { get; set; }
+    }
+
+    public class ViewBurialsFilterModel
+    {
+        public string NS { get; set; }
+        public int NSTop { get; set; }
+        public int NSBottom { get; set; }
+        public string EW { get; set; }
+        public int EWTop { get; set; }
+        public int EWBottom { get; set; }
+        public string Quadrant { get; set; }
+
+        public string Hair { get; set; }
+        // continue with all the different things that they could filter on
+    }
+}
